a91a8975caf179538951868ab3e5ddf8ed679f27
ehsan2793 <ehsan2793@gmail.com>
2021-10-03 00:47:53 -0700

    table router and seed data are made

--- api/server/server.js
+++ api/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const nameRouter = require('../nameRouter/Router');
 
 const server = express();
@@ -6,13 +7,15 @@ const server = express();
 server.use(express.json())
 
 
+server.use('/api/names', nameRouter)
 
 server.get('/', (req, res) => {
     res.status(200).send("hello world")
 
 })
 
-server.use('/api/names', nameRouter)
+
+
 
 
 
